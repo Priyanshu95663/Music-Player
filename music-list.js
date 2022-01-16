626fa7e0f57afeb7cb6a10597b453f41836c6894
Priyanshu_M <72194769+Priyanshu95663@users.noreply.github.com>
2022-01-17 00:11:05 +0530

    firstcommit

---
+++ music-list.js
@@ -0,0 +1,40 @@
+
+let allMusic = [
+    {
+        name: "Hanuman Chalisa",
+        artist: "Gulshan Kumar",
+        img: "music-1",
+        src: "music-1"
+    },
+    {
+        name: "Namo Namo",
+        artist: "Audio Library",
+        img: "music-2",
+        src: "music-2"
+    },
+    {
+        name: "Dance Monkey",
+        artist: "Tones & I",
+        img: "music-3",
+        src: "music-3"
+    },
+    {
+        name: "Phid se Ud Chala",
+        artist: "Mohit Chauhan",
+        img: "music-4",
+        src: "music-4"
+    },
+    {
+        name: "Dynamite",
+        artist: "BTS",
+        img: "music-5",
+        src: "music-5"
+    },
+    {
+        name: "Lovely",
+        artist: "Billie Ellish",
+        img: "music-6",
+        src: "music-6"
+    },
+];
+
