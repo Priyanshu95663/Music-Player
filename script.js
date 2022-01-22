610f9d6b2adee4234af15c0a55274f4a237c6929
Priyanshu_M <72194769+Priyanshu95663@users.noreply.github.com>
2022-01-22 13:19:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
 
 canvas.width = innerWidth
-canvas.height = innerWidth
+canvas.height = innerHeight
 
 const mouse = {
     x: innerWidth / 2,
